ec93963200d182d56680c2443fafe1dc7f051125
Harut <harut@Haruts-MacBook-Air.local>
2025-12-29 02:06:06 +0400

    Added comments

--- Src/main.py
+++ Src/main.py
@@ -1,3 +1,5 @@
+# Imports, Path for DB and API URL
+
 import json
 import sqlite3
 import requests
@@ -9,19 +11,27 @@ BASE_URL = "https://api.open-meteo.com/v1/forecast"
 DB_PATH = Path("../Data/DB/AERO.db")
 
 
+# ________________________________________________________________________________________________________________________________________________________
+# City names and coordinates
+
 latlong = {"London":[51.50, -0.12],"New York": [40.71, -74.00],"Tokyo": [35.67, 139.65],"Sydney": [-33.86, 151.20],"Reykjavik": [64.14, -21.89]}
 cities = ["London", "New York", "Tokyo", "Sydney", "Reykjavik"]
-data1 = []
+
+
+# _________________________________________________________________________________________________________________________________________________________
+# Data collection
+
+data = []
 
 for lat, long in latlong.values():
     resp = requests.get(f"{BASE_URL}?latitude={lat}&longitude={long}&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&forecast_days=3").json()
     resp["city"] = [n for n, l in latlong.items() if l == [lat, long]][0]
-    data1.append(resp)
+    data.append(resp)
     
 
 rows = []
 
-for di in data1:
+for di in data:
     city = di["city"]
     hourly = di["hourly"]
 
@@ -39,6 +49,9 @@ for di in data1:
             "wind_kph": winds[i]
         })
 
+
+# _________________________________________________________________________________________________________________________________________________________
+# DB creation and data insertion
 
 conn = sqlite3.connect(DB_PATH)
 cur = conn.cursor()
@@ -67,10 +80,13 @@ com_for_insert = """
     VALUES(:city, :forecast_time, :temp_c, :humidity, :wind_kph)
     """
 
-for row in rows:
-    cur.execute(com_for_insert, row)
+cur.executemany(com_for_insert, rows)
 
 conn.commit()
+
+
+# ______________________________________________________________________________________________________________________________________________________________
+# Pandas data reading
 
 df = pd.read_sql("""SELECT * FROM forecasts""", conn)
 conn.close()
@@ -78,18 +94,20 @@ conn.close()
 groupt = df.groupby("city")["temp_c"].max()
 
 
+# ______________________________________________________________________________________________________________________________________________________________
 
+# Question 1
 h_temp = groupt.max()
 h_temp_city = groupt.idxmax()
 print(f"Highest predicted tempreture in next 3 days has {h_temp_city} with tempreture of {h_temp:.2f}")
 
-
+# Question 2
 groupws = df.groupby("city")["wind_kph"].mean()
 h_ws = groupws.max()
 h_ws_city = groupws.idxmax()
 print(f"Highest average wind speed in next 3 days has {h_ws_city} with speed of {h_ws:.2f}")
 
-
+# Question 3
 groupht = groupt = df.groupby("city")["temp_c"].max()
 grouplt = groupt = df.groupby("city")["temp_c"].min()
 temp_swing = {}
@@ -102,7 +120,7 @@ h_ts = max(temp_swing.values())
 h_ts_city = [city for city, swg in temp_swing.items() if swg == h_ts][0]
 print(f"Highest tempreture swing in next 3 days has {h_ts_city} with speed of {h_ts:.2f}")
 
-
+# Question 4
 groupat = df.groupby("city")["temp_c"].mean()
 
 for city in cities:
@@ -110,6 +128,7 @@ for city in cities:
     print(f"Average predicted tempreture in {city} is {avg_temp:.2f}")
     
     
+# Question 5
 df["fog_rain_risk"] = np.ones(360)
 df["fog_rain_risk"] = df["fog_rain_risk"].where(df["humidity"]>90, 0)
 
